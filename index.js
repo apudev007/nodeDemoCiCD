726e01ce9968f6be6f67a67832c90e95e00e7832
kar_apu <apu@karmicksolutions.com>
2023-01-19 17:12:29 +0530

    changes done

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log("Hello World...!");
+const express = require("express")
+const app = express();
+const port  = process.env.PORT || 3000;
+app.get("/", (req, res)=>{
+    res.send("Hi, There!");
+})
+app.listen(port, ()=>{
+    console.log(`app is running on ${port}`);
+})
